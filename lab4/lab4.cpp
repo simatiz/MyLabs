072b546f0828a96eee094ff70b7191f0689042ce
simatiz <simtchyk@gmail.com>
2021-10-25 22:23:39 +0300

    done

---
+++ lab4/lab4.cpp
@@ -0,0 +1,21 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <cmath>
+#include <stdio.h>
+
+using namespace std;
+int main() {
+	int n, k=0, a;
+	cin >> n;
+	a = n;
+	while (n / 10 != 0) {
+		n = n / 10;
+		k++;
+	}
+	for (int i = 1; i <= k; i++) {
+		n += a % 10;
+		a = a / 10;
+	}
+	cout << n;
+	return 0;
+}
