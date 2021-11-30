881e27bbd59d0ad19ed75c3d11df5346685c56fd
simatiz <simtchyk@gmail.com>
2021-11-30 18:46:07 +0200

    lab 8.2 done

---
+++ lab8.2/lab8.2.cpp
@@ -0,0 +1,78 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <cmath>
+#include <stdio.h>
+#include <algorithm>
+using namespace std;
+
+static void BubbleSort(int** a, int n) {
+	bool flag = true;
+	int el;
+	while (flag) {
+		for (int i = 0; i < n-1; i++) {
+			flag = false;
+			if (a[i][i] > a[i+1][i+1]) {
+				el = a[i][i];
+				a[i][i] = a[i+1][i+1];
+				a[i+1][i+1] = el;
+				flag = true;
+			}
+		}
+	}
+}
+
+static void Swap(int** a, int** b, int n) {
+	int el;
+	for (int i = 0; i < n; i++) {
+		el = a[i][i];
+		a[i][i] = b[i][i];
+		b[i][i] = el;
+	}
+}
+
+int main() {
+	int n;
+	cin >> n;
+
+	int** a = new int* [n];
+	for (int i = 0; i < n; i++) a[i] = new int[n];
+	int** b = new int* [n];
+	for (int i = 0; i < n; i++) b[i] = new int[n];
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> a[i][j];
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> b[i][j];
+		}
+	}
+	cout << endl;
+	
+	BubbleSort(a, n);
+	BubbleSort(b, n);
+
+	Swap(a, b, n);
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << a[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << b[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	delete[] a;
+	delete[] b;
+	return 0;
+}
